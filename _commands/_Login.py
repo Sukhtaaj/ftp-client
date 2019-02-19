26d0da02b69f9af4af7c231ad52fc9cb8cf3a234
Sukhdeep Singh <sukhdeep@pdx.edu>
2019-02-19 01:11:56 -0800

    Add Login Feature (Sukhdeep's story)

---
+++ _commands/_Login.py
@@ -0,0 +1,27 @@
+import getpass
+
+class Command:
+    def do_login(self, *args):
+        """
+        Command to login with user and password in the connected FTP host.
+        """
+        print "Press Enter for selecting default values\n"
+
+        while not self._hostname:
+            self._hostname = raw_input('Host (default: test.rebex.net):') or 'test.rebex.net'
+        
+        while not self._username:
+            self._username = raw_input('Username(default= demo): ') or 'demo'
+
+        while not self._password:
+            self._password = getpass.getpass('Password(default= password): ') or 'password'
+
+        response = self._perform_ftp_command('Connection', self._hostname, self._username, None, self._password)
+
+        if isinstance(response, str):
+            print response
+        else:
+            print "Logged in successfully"
+            self._connection_object = response
+
+        self._update_prompt()
