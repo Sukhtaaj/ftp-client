5ec9f64a2b6fc32e385bc1315b97f403e6081b35
victcab <victorcabunoc@gmail.com>
2019-02-26 19:35:54 -0800

    Added user selection

--- _commands/_ConnectionInfo.py
+++ _commands/_ConnectionInfo.py
@@ -4,25 +4,34 @@ class Command():
         Command to display saved connections to user
 
         """
-        i = 1;
-
+        i = 0
         text_file = open("_commands/info.csv", "r")
+
         for line in text_file:
-            #print(i + ")")
             fields = line.split(",")
             host = fields[0]
             username = fields[1]
-            print("{}{}" + " " + host + " " + username).format(i,")" )
+            print("{}{}" + " " + host + " | " + username).format(i+1,")" )
             i = i+1
 
-        '''
-        lines = text_file.read().split(';')
-        print lines
-        print len(lines)
+        selection = input("Selection: ")
+
+        text_file.seek(0)
+        j = 0
+        for line in text_file:
+            fields = line.split(",")
+            host = fields[0]
+            username = fields[1]
+            if(j == selection-1):
+                break
+            else:
+                j = j+1
         text_file.close()
-        '''
         
-        self._hostname = "babbage.cs.pdx.edu"
-        self._username= "cabunoc"
+        self._hostname = host
+        self._username = username
+
+        print(self._hostname)
+        print(self._username)
 
         return
