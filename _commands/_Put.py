07ffdc5e0a20591bfee4d0796380892e59b5c2c8
DivyaSravaniVukkusila <45480789+DivyaSravaniVukkusila@users.noreply.github.com>
2019-02-28 19:00:15 -0800

    Update _Put.py

--- _commands/_Put.py
+++ _commands/_Put.py
@@ -5,13 +5,13 @@ class Command:
         Args:
             fileName (str): File to upload into current working directory of the remote host.
         """
-		file_list = localPath.split(" ")
-		if len(file_list) < 1:
+	file_list = localPath.split(" ")
+	if len(file_list) < 1:
             print ("Atleast one file/path expected")
         else:
-			try:
-				for file in file_list:
-					response = self._perform_ftp_command('put', file)
-				print "File(s) uploaded Successfully"
-			except IOError as e:
-              print(e)		
+	    try:
+	        for file in file_list:
+		       response = self._perform_ftp_command('put', file)
+		print "File(s) uploaded Successfully"
+	    except IOError as e:
+            print(e)		
