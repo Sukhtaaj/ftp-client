32925a1086bca861ef7842842260bc52ad379e55
pallavigusain92 <47799976+pallavigusain92@users.noreply.github.com>
2019-02-28 18:15:06 -0800

    Updated
    Get multiple

--- _commands/_getmultiple.py
+++ _commands/_getmultiple.py
@@ -6,11 +6,12 @@ class Command:
         Args:
             filepath - path of the file to be deleted from the remote server.
         """
-		file_path = remotepath.split(" ")
-		if len(remote_src_dest)!=2:
-			print ("Enter remotepath and localpath")
+		file_list= remotepath.split(" ")
+		if len(file_list) < 1:
+			print ("Atleast one file/path expected")
 		else:
 		    try: 
-		      response = self._perform_ftp_command('get',file_path[0], file_path[1])
+			for file in file_list:
+ 		            response = self._perform_ftp_command('get', file)
 		    except IOError as e:
 			  print(e)
