60b8e8b4b9e0818c2748bc18daf7931a4e5446c9
Daniel Lee <danlee@augusta.cs.pdx.edu>
2019-03-07 15:54:54 -0800

    removed createdirectory as it was a duplicate of mkdir

--- wrapper.py
+++ wrapper.py
@@ -7,7 +7,6 @@ import _commands._Logout
 import _commands._List
 import _commands._Exit
 import _commands._Chdir
-import _commands._CreateDirectory
 import _commands._Rename
 import _commands._Llist
 import _commands._Lrename
@@ -22,7 +21,7 @@ from paramiko import (SSHException, AuthenticationException,
 from pysftp.exceptions import (CredentialException, ConnectionException,
                                HostKeysException)
 
-class SFTPWrapper(Cmd, _commands._Login.Command, _commands._Logout.Command, _commands._List.Command, _commands._Exit.Command, _commands._Chdir.Command, _commands._CreateDirectory.Command, _commands._Rename.Command, _commands._Llist.Command,  _commands._Lrename.Command, _commands._Mkdir.Command, _commands._Put.Command, _commands._ConnectionInfo.Command, _commands._delete.Command, _commands._getmultiple.Command):
+class SFTPWrapper(Cmd, _commands._Login.Command, _commands._Logout.Command, _commands._List.Command, _commands._Exit.Command, _commands._Chdir.Command, _commands._Rename.Command, _commands._Llist.Command,  _commands._Lrename.Command, _commands._Mkdir.Command, _commands._Put.Command, _commands._ConnectionInfo.Command, _commands._delete.Command, _commands._getmultiple.Command):
     """
     FTP client command line utility.
     """
