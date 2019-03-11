d72f5d9772755be9a44102a478515af6db41c4f8
Sukhdeep Singh <sukhdeep@pdx.edu>
2019-03-11 00:34:46 -0700

    Implement Chmod command

---
+++ _commands/_Chmod.py
@@ -0,0 +1,20 @@
+import sys, os
+class Command:
+    def do_chmod(self, input_args):
+        """
+        Command to change permissions for file/directory 
+
+	Args:
+             file (str): filename or path 
+	     permissions (int): permissions to grant e.g. 755, 644, or 000.
+        """
+	if not input_args:
+		print("Filename and permissions expected")
+	else:
+          if len(input_args.split(" ")) == 2:
+                filepath = input_args.split(" ")[0]
+                permission = input_args.split(" ")[1]
+		directory = self._perform_ftp_command('chmod', filepath, permission)
+          else:
+                print("Exactly two arguments expected")
+
--- wrapper.py
+++ wrapper.py
@@ -17,13 +17,14 @@ import _commands._ConnectionInfo
 import _commands._delete
 import _commands._Get
 import _commands._History
+import _commands._Chmod
 
 from paramiko import (SSHException, AuthenticationException, 
                       PasswordRequiredException)
 from pysftp.exceptions import (CredentialException, ConnectionException,
                                HostKeysException)
 
-class SFTPWrapper(Cmd, _commands._Login.Command, _commands._Logout.Command, _commands._List.Command, _commands._Exit.Command, _commands._Chdir.Command, _commands._Rename.Command, _commands._Llist.Command,  _commands._Lrename.Command, _commands._Mkdir.Command, _commands._Put.Command, _commands._ConnectionInfo.Command, _commands._delete.Command, _commands._Get.Command, _commands._History.Command):
+class SFTPWrapper(Cmd, _commands._Login.Command, _commands._Logout.Command, _commands._List.Command, _commands._Exit.Command, _commands._Chdir.Command, _commands._Rename.Command, _commands._Llist.Command,  _commands._Lrename.Command, _commands._Mkdir.Command, _commands._Put.Command, _commands._ConnectionInfo.Command, _commands._delete.Command, _commands._Get.Command, _commands._History.Command, _commands._Chmod.Command):
     """
     FTP client command line utility.
     """
